817c269c1c77754ad92f9d89048e38116e71f293
yehonatancohen <yoncohenyon@gmail.com>
2026-03-17 18:34:06 +0200

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Run database migrations
 npx prisma migrate deploy
 
+# Remove stale Chromium lock files from previous containers
+find /app/.wwebjs_auth -name 'SingletonLock' -o -name 'SingletonSocket' -o -name 'SingletonCookie' 2>/dev/null | xargs rm -f || true
+
 # Execute the main command
 exec "$@"
